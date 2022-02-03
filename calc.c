fa4c761bae639cd79dc780853f637b39d074f9b9
Mindaugas Ramonas <mindaugas.ramonas@yahoo.com>
2022-02-03 22:46:14 +0200

    bool type removed, event loop added

--- calc.c
+++ calc.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main(void)
 {
+    char ch;
+
+    while(1)
+    {
+        printf("Enter RPN expression: ");
+
+        while((ch = getchar()) != EOF && ch != '\n')
+        {
+            processCharacter(ch);
+        }
+    }
     return 0;
 }
